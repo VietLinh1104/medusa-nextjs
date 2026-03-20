6a06eddb7d6f93b1ec71097157b0d52126dd287a
VietLinh1104 <linhtx2004@gmail.com>
2026-03-21 00:44:56 +0700

    [fix] gap product

--- modules/home/components/product-grid/index.tsx
+++ modules/home/components/product-grid/index.tsx
@@ -25,7 +25,7 @@ const ProductGrid = ({ title, subtitle }: { title: string, subtitle: string }) =
           </Link>
         </div>
 
-        <div className="grid grid-cols-2 gap-y-12 gap-x-8 sm:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-y-6 gap-x-4 md:gap-y-12 md:gap-x-8 md:grid-cols-3 lg:grid-cols-4">
           {dummyProducts.map((product) => (
             <ProductCard key={product.id} {...product} />
           ))}
